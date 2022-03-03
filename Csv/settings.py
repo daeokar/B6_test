09ff643fb6ffbb695c49725eaabb546ddbcc83b2
daeokar <darokar.aniket1999@gmail.com>
2022-03-03 12:35:50 +0530

    add pythonanywhere mysql database4

--- Csv/settings.py
+++ Csv/settings.py
@@ -81,28 +81,28 @@ WSGI_APPLICATION = 'Csv.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'Csv',
-        'USER': 'root', 
-        'PASSWORD': '123456',
-        'HOST': 'localhost',   
-        'PORT': '3306',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'Aniket24$B6_db',
-#         'USER': 'Aniket24',
-#         'PASSWORD': 'Anu24011999',
-#         'HOST': 'Aniket24.mysql.pythonanywhere-services.com',
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'Csv',
+#         'USER': 'root', 
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',   
 #         'PORT': '3306',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Aniket24$B6_db',
+        'USER': 'Aniket24',
+        'PASSWORD': 'Anu24011999',
+        'HOST': 'Aniket24.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
